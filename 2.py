1cb40e2fa5ebd7053921b9f0ce2c549aeb756488
Aadith82 <aadithps828@gmail.com>
2025-03-07 14:08:29 +0530

    Add files via upload
    technical training

---
+++ 2.py
@@ -0,0 +1,3 @@
+temc=float(input())
+temf=((9*temc)/5)+32
+print(round(temf,1))
---
+++ 4.py
@@ -0,0 +1,7 @@
+n=float(input())
+k=0
+for i in range(int(n)):
+    a=float(input())
+    k+=a
+l=k/n
+print('The average is: {:.2f}'.format(l))
